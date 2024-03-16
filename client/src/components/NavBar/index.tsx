c4495b5e7fc1420d01c9dff4dd7f0e3ba9294a1c
Bersival <bersivalit@gmail.com>
2024-03-16 23:24:58 +0500

    feat[navbar]: Added a href with components anchors

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
 import styles from './styles.module.scss'
+import {Link} from 'react-scroll'
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = props => {
-  const navItems = ['About me', 'Stack', 'Projects', 'Links']
-  const [activeToken, setActiveToken] = useState<string>('About me')
+  const navItems = ['About me', 'Stack', 'Experience', 'Links'] // titles
+  const navLinks = ['about', 'stack', 'experience', 'footer'] // anchors
+
+  // const [activeToken, setActiveToken] = useState<string>('About me')
+
   return (
     <div className={styles.navBarWrapper}>
       <nav
@@ -15,12 +19,12 @@ export const NavBar: React.FC<NavBarProps> = props => {
           {navItems.map((item, index) => (
             <li
               key={index}
-              onClick={() => setActiveToken(item)}
-              className={styles['nav-item'] + ' ' + (item === activeToken ? styles['active'] : '')}>
-              {item}
+              // onClick={() => setActiveToken(item)}
+              className={styles['nav-item'] + ' ' + (item === '' ? styles['active'] : '')}>
+              <a href={`#${navLinks[index]}`}>{item}</a>
             </li>
           ))}
-          <div className={styles.indicator}></div>
+          {/* <div className={styles.indicator}></div> */}
         </ul>
       </nav>
     </div>
